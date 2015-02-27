5502e2c2df02a66fd584ccb8e36652515f581936
Adam Cooper <amcooper@gmail.com>
2015-02-27 16:39:04 -0500

    Works for Flatbush.

--- public/js/fp_main.js
+++ public/js/fp_main.js
@@ -1,5 +1,17 @@
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
+var sampleRoute = [{stationName: "Lorimer St", latitude: 40.714067, longitude: -73.950275},
+  {stationName: "Graham Av", latitude: 40.714588, longitude: -73.944031}, 
+  {stationName: "Grand St", latitude: 40.711904, longitude: -73.940641}, 
+  {stationName: "Montrose Av", latitude: 40.707732, longitude: -73.939868}, 
+  {stationName: "Morgan Av", latitude: 40.706138, longitude: -73.933152}, 
+  {stationName: "Jefferson St", latitude: 40.706609, longitude: -73.922959}, 
+  {stationName: "DeKalb Av", latitude: 40.703828, longitude: -73.918378}, 
+  {stationName: "Myrtle-Wyckoff Avs", latitude: 40.699826, longitude: -73.911608}, 
+  {stationName: "Halsey St", latitude: 40.695572, longitude: -73.904120}, 
+  {stationName: "Wilson Av", latitude: 40.688763, longitude: -73.904012}, 
+  {stationName: "Bushwick Av - Aberdeen St", latitude: 40.682808, longitude: -73.905257}, 
+  {stationName: "Broadway Jct", latitude: 40.678862, longitude: -73.903272}];
 
 function initialize () {
 	var mapProp = {
